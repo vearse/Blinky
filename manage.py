c2ac1b6825113fc222145bf2dd0df45c38e0f39f
vearse <versatilekaywize94@gmail.com>
2022-07-31 17:01:00 +0100

    Manager

--- manage.py
+++ manage.py
@@ -119,6 +119,6 @@ def format():
 
 
 if __name__ == '__main__':
-    Server(host="0.0.0.0", port="8080")
-    app.run()
+    # Server(host="0.0.0.0", port="8080")
+    app.run(host='0.0.0.0',port=8080)
 
